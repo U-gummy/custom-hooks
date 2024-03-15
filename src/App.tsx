e4e2496daf80a7f4fb2b735b69c3be2a85809b99
yougmi <zoz7353@gmail.com>
2024-03-15 12:44:57 +0900

    :heavy_plus_sign: add use toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useToast } from "@u-gummy/use-toast";
 
 function App() {
+  const { toasts, showToast } = useToast();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          showToast({
+            title: "title입니다",
+            type: "success",
+            message: "이렇게 저렇게 되었습니다.",
+          });
+        }}
+      >
+        toast
+      </button>
+      <button
+        onClick={() => {
+          showToast({
+            type: "success",
+            message: "이렇게 111212 되었습니다.",
+          });
+        }}
+      >
+        toast
+      </button>
+      <button
+        onClick={() => {
+          showToast({
+            type: "success",
+            message: "이렇게 12122 되었습니다.",
+          });
+        }}
+      >
+        toast
+      </button>
+      {toasts &&
+        toasts.map(({ title, message, id }) => (
+          <div key={id}>
+            {title} {message}
+          </div>
+        ))}
+    </>
   );
 }
 
